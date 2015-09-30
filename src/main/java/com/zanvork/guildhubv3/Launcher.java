14341a3c144fa26af6223b23badb331f13a6592d
zanvork <jamie.greeman@gmail.com>
2015-09-30 11:47:48 +0100

    Inital commit 	-	created pom with required spring boot dependencies 	-	added launcher with main method

---
+++ src/main/java/com/zanvork/guildhubv3/Launcher.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zanvork.guildhubv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author jgreeman
+ */
+@SpringBootApplication
+public class Launcher {
+       
+    public static void main(String args[]) {
+        SpringApplication.run(Launcher.class);
+    }
+}
