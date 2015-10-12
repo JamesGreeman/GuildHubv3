41daa9127e59b4d7f336b3ff0c83eaf7be303734
zanvork <jamie.greeman@gmail.com>
2015-10-12 16:34:31 +0100

    Configured tomcat to use https for all requests

--- src/main/java/com/zanvork/guildhubv3/Launcher.java
+++ src/main/java/com/zanvork/guildhubv3/Launcher.java
@@ -1,8 +1,10 @@
 package com.zanvork.guildhubv3;
 
+import com.zanvork.guildhubv3.configuration.TomcatConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -12,9 +14,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan({"com.zanvork"})
+@Import(TomcatConfiguration.class)
 public class Launcher {
        
     public static void main(String args[]) {
         SpringApplication.run(Launcher.class);
     }
+    
 }
